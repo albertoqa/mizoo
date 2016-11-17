bbbc4de45bc9c8957b1f47a6a32fda94397a99bd
Alberto Quesada <qa.alberto@gmail.com>
2016-11-18 00:38:11 +0100

    set up for pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'mizoo',
+  packages = ['mizoo'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Use deep learning to rename your photos.',
+  author = 'Alberto Quesada',
+  author_email = 'qa.alberto@gmail.com',
+  url = 'https://github.com/albertoqa/mizoo.git', # use the URL to the github repo
+  download_url = 'https://github.com/albertoqa/mizoo.git/tarball/0.1',
+  keywords = ['captioning', 'image', 'ai', 'deep learning'],
+  classifiers = [],
+  license="MIT"
+)
